49725252df61d20d61e91d1f75e93364fe935d5e
Bowen Li <thestateofmay@wireless-10-104-191-177.umd.edu>
2017-09-04 18:13:59 -0400

    login + register page

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,40 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import LoginScreen from './Loginscreen';
+import UploadScreen from './UploadScreen';
+import UploadPage from './UploadPage';
+import React, { Component } from 'react';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      loginPage:[],
+      uploadScreen:[]
+    }
+  }
+  componentWillMount(){
+    var loginPage =[];
+    loginPage.push(<LoginScreen appContext={this}/>);
+    this.setState({loginPage:loginPage})
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {this.state.loginPage}
+        {this.state.uploadScreen}
       </div>
     );
   }
 }
+
+const style = {
+  margin: 15,
+};
 
 export default App;
