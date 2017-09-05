b5e93db9ce53a938c42ccc02924e549220899d25
Nathan Hefner <hefner.nathan@gmail.com>
2017-09-04 20:01:05 -0400

    Somebody call the vet my dog turned out to be a rat

--- server/src/schema.js
+++ server/src/schema.js
@@ -18,6 +18,12 @@ input CreateBudgetInput {
 	amtAllowed: Float!
 }
 
+input CreateTransactionInput {
+  type: String!
+  description: String!
+  amount: Float!
+}
+
 type User {
 	id: ID!
 	email: String!
@@ -47,7 +53,7 @@ type Token {
 }
 
 type Query {
-	user(id: ID!): User 
+	user(id: ID!): User
   	transactions(userId: ID!): [Transaction]
   	budget(userId: ID!): Budget
 }
@@ -56,6 +62,7 @@ type Mutation {
 	signup(user: SigninUserInput!): User
 	login(user: SigninUserInput!): Token
 	createBudget(budget: CreateBudgetInput!): Budget
+  createTransaction(transaction: CreateTransactionInput!): Transaction
 	updateTransaction(id: ID!): Transaction
 }
 `
