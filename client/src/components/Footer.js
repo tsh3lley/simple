d35528ccdcf751c94941aa77f24a5b0295f140c1
the kid <thomasjshelley@gmail.com>
2017-09-19 22:58:12 -0700

    added shitty footer that doesnt work

---
+++ client/src/components/Footer.js
@@ -0,0 +1,28 @@
+import FontAwesome from 'react-fontawesome';
+import colors from '../lib/colors';
+import React, { Component } from 'react';
+
+const Footer = () => (
+  <footer className='container'>
+    <div className='container grid-lg text-center'>
+      <div>
+        <p className='m-0'>
+          <span>Made in &nbsp;</span>
+          <FontAwesome name='fa fa-sun-o' style={{ color: '#DAA520' }} />
+          <span>&nbsp;s u n n y&nbsp;</span>
+          <FontAwesome name='fa fa-sun-o' style={{ color: '#DAA520' }} />
+          <span>&nbsp;in San Francisco.</span>
+        </p>
+      </div>
+    </div>
+    <style jsx>{`
+      footer {
+        border-top: 1px solid ${colors.grey};
+        padding-top: 20px;
+        padding-bottom: 20px;
+      }
+    `}</style>
+  </footer>
+);
+
+export default Footer;
